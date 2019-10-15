235e5cae0355455efe87d0a9df5e26ea70edec87
Rodrigo Oliveira <rodrigocode4@gmail.com>
2019-10-15 20:24:13 -0300

    finished function chapter

---
+++ funcoes/decorator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+
+def log(function):
+    def decorator(*args, **kwargs):
+        print(f"Início da chamada da função: {function.__name__}")
+        print(f"args: {args}")
+        print(f"kwargs: {kwargs}")
+        resultado = function(*args, **kwargs)
+        print(f"Resultado da chamada: {resultado}")
+        return resultado
+
+    return decorator
+
+@log
+def soma(x, y):
+    return x + y
+
+@log
+def sub(x, y):
+    return x - y
+
+
+if __name__ == '__main__':
+    print(soma(2,3))
+    print(sub(10, y=2))
---
+++ funcoes/desafio_gerador_html_v6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def tag(tag, *args, **kwargs):
+    if "html_class" in kwargs:
+        kwargs["class"] = kwargs.pop("html_class")
+    attrs = "".join(f"{key}={value}" for key, value in kwargs.items())
+    inner = "".join(args)
+    return f"<{tag} {attrs}>{inner}</{tag}>"
+
+
+if __name__ == "__main__":
+    print(
+        tag("p",
+            tag("span", "Curso de Python 3, por"),
+            tag("strong", "Jurandir Filho", id="jf"),
+            tag("span", " e "),
+            tag("strong", "Leonardo Leitão", id='ll'),
+            tag("span", "."),
+            html_class="alert",
+        )
+    )
